0aa401a04d3c17f64d68037e279277a5041a8cd5
= <=>
2024-04-24 19:49:12 +0700

    Commit

--- script.js
+++ script.js
@@ -20,30 +20,3 @@ function moreButton(){
      var contentElement = document.getElementById('Message');
     contentElement.scrollIntoView({ behavior: 'smooth' });
 }
-function mouseEnter(button){
-    button.style.backgroundColor = "rgba(239,239,239,0.6)"
-    button.style.color = "gray"
-}
-function mouseLeave(button){
-    button.style.backgroundColor = "rgba(255,255,255,0.2)"
-    button.style.color = "black"
-}
-
-function learnMoreButtonEntered(button){
-    button.style.backgroundColor = "rgba(224,224,224,0.6)"
-}
-function learnMoreButtonLeave(button){
-    button.style.backgroundColor = "rgba(255,255,255,0.2)"
-}
-function enterDropdown(button){
-    button.style.backgroundColor = "rgb(192,192,192)"
-}
-function leaveDropdown(button){
-    button.style.backgroundColor = "rgb(255,255,255)"
-}
-function enterContact(button){
-    button.style.backgroundColor = "rgb(251, 70, 118)"
-}
-function leaveContact(button){
-    button.style.backgroundColor = "rgb(231, 30, 78)"
-}
